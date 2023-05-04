755304033f037bc569bf4076f58449d0ea77e7f3
Mai Đức Thắng <95439100+Tieu-Mai-Nhi@users.noreply.github.com>
2023-05-04 16:55:52 +0700

    Update Home.js
    ok

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withSpring, withTiming } from 'react-native-reanimated';
 
-const SIZE = 100;
+const SIZE = 106;
 export default function Home() {
     const progress = useSharedValue(1);
     const scale = useSharedValue(1);
