f3843bbdac7d814378b3b864412573efdd65d116
Mai Duc Thang <thangmdfx12498@funix.edu.vn>
2023-05-04 14:10:47 +0700

    C5

---
+++ src/screens/Git.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Git = () => {
+  return (
+    <View>
+      <Text>Git</Text>
+    </View>
+  );
+};
+
+export default Git;
+
+const styles = StyleSheet.create({});
