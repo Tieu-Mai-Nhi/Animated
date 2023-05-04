61e1bcb0f6876a8639c2d0c7322c7672107253e4
Mai Duc Thang <thangmdfx12498@funix.edu.vn>
2023-05-04 14:34:12 +0700

    Fix C9 alpha

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,45 +1,51 @@
-import React, { useEffect } from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withSpring, withTiming } from 'react-native-reanimated';
+import React, { useEffect } from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
 
-const SIZE = 100;
+const SIZE = 103;
 export default function Home() {
-    const progress = useSharedValue(1);
-    const scale = useSharedValue(1);
+  const progress = useSharedValue(1);
+  const scale = useSharedValue(1);
 
-    const reanimatedStyle = useAnimatedStyle(() => {
-        return {
-            opacity: progress.value,
-            borderRadius: progress.value * SIZE / 2,
-            transform: [
-                { scale: scale.value },
-                { rotate: `${progress.value * 2 * Math.PI}rad` }
-            ]
-        };
-    }, [])
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value,
+      borderRadius: (progress.value * SIZE) / 2,
+      transform: [
+        { scale: scale.value },
+        { rotate: `${progress.value * 2 * Math.PI}rad` },
+      ],
+    };
+  }, []);
 
-    useEffect(() => {
-        progress.value = withRepeat(withTiming(0.5), -1, true);
-        scale.value = withRepeat(withSpring(2), -1, true);
-    }, [])
+  useEffect(() => {
+    progress.value = withRepeat(withTiming(0.5), -1, true);
+    scale.value = withRepeat(withSpring(2), -1, true);
+  }, []);
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <Animated.View style={[
-                { height: SIZE, width: SIZE, backgroundColor: 'blue' },
-                reanimatedStyle,
-            ]}
-            />
-
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={styles.container}>
+      <Animated.View
+        style={[
+          { height: SIZE, width: SIZE, backgroundColor: "blue" },
+          reanimatedStyle,
+        ]}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ccc',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#ccc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
