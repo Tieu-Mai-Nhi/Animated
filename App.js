192fbb74722cb0dde82dad089f4d84e08e880cb6
Mai Duc Thang <thangmdfx12498@funix.edu.vn>
2023-05-04 14:03:55 +0700

    C3

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import Bell from './src/screens/Bell';
-import Home from './src/screens/Home';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import { StyleSheet } from "react-native";
+import Home from "./src/screens/Home";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Bell" screenOptions={{
-        headerShown: false
-      }}>
-        {/* <Stack.Screen name="Home" component={Home} /> */}
-        <Stack.Screen name="Bell" component={Bell} />
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
